b7dbbb82a16b6afd81bce5bff033a2132cacd020
Aljinas-XYN105 <aljinas@xynosoft.com>
2024-04-03 09:40:50 +0000

    fixes

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,22 +5,40 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\Tenant;
 use Illuminate\Support\Facades\Auth;
-use Validator;
+use App\Traits\ApiResponser;
+use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
 class AuthController extends BaseController
 {
+    use ApiResponser;
     public function signin(Request $request)
     {
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $authUser = Auth::user();
-            $success['token'] =  $authUser->createToken('MyAuthApp')->plainTextToken;
-            $success['name'] =  $authUser->name;
-
-            return $this->sendResponse($success, 'User signed in');
+            $success['api_token'] =  $authUser->createToken('MyAuthApp')->plainTextToken;
+            $success['email'] =  $authUser->email;
+            $success['user'] =  $authUser;
+            $success['tenant'] =  Tenant::whereId($authUser->tenant_id)->first();
+            return $this->success($success, "User signed in.", 200);
         } else {
-            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
+            return $this->error('Unauthorised', 401);
+        }
+    }
+
+    public function verifyToken(Request $request)
+    {
+        $authUser = Auth::user();
+        if (isset($authUser)) {
+            $success['api_token'] =  $request['api_token'];
+            $success['email'] =  $authUser->email;
+            $success['user'] =  $authUser;
+            $success['tenant'] =  Tenant::whereId($authUser->tenant_id)->first();
+            return $this->success($success, "User signed in.", 200);
+        } else {
+            return $this->error('Unauthorised', 401);
         }
     }
 
@@ -39,8 +57,8 @@ class AuthController extends BaseController
 
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
-        
-        
+
+
         try {
             $user = User::create($input);
             $success['token'] =  $user->createToken('MyAuthApp')->plainTextToken;
@@ -48,10 +66,10 @@ class AuthController extends BaseController
             return $this->sendResponse($success, 'User created successfully.');
         } catch (\Illuminate\Database\QueryException $e) {
             $errorCode = $e->errorInfo[1];
-            if ($errorCode == 1062) { 
+            if ($errorCode == 1062) {
                 return $this->sendError('Error validation', ['email' => 'Email already exists.']);
             }
-           
+
             return $this->sendError('Database error', ['error' => $e->getMessage()]);
         }
     }
